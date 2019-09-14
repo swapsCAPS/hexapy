2b7da11072cf0d5acd100f2e4a7214563f531a80
swapsCAPS <verstegen.daan@gmail.com>
2019-09-14 12:57:30 +0100

    structure

--- main.py
+++ main.py
@@ -4,35 +4,145 @@ from adafruit_servokit import ServoKit
 left  = ServoKit(channels=16, address=0x40)
 right = ServoKit(channels=16, address=0x41)
 
+front_right = [
+	{
+		"index": 0,
+		"pulse_width_range": [ 1600, 2500 ],
+		"actuation_range":   90,
+	},
+	{
+		"index": 1,
+		"pulse_width_range": [ 1200, 2750 ],
+		"actuation_range":   180,
+	},
+	{
+		"index": 2,
+		"pulse_width_range": [ 500, 2000 ],
+		"actuation_range":   130,
+	},
+]
+
+mid_right = [
+	{
+		"index": 3,
+		"pulse_width_range": [ 2200, 2500 ],
+		"actuation_range":   90,
+	},
+	{
+		"index": 4,
+		"pulse_width_range": [ 550, 2500 ],
+		"actuation_range":   180,
+	},
+	{
+		"index": 5,
+		"pulse_width_range": [ 600, 2000 ],
+		"actuation_range":   130,
+	},
+]
+
+back_right = [
+	{
+		"index": 6,
+		"pulse_width_range": [ 550, 1300 ],
+		"actuation_range":   90,
+	},
+	{
+		"index": 7,
+		"pulse_width_range": [ 750, 2500 ],
+		"actuation_range":   180,
+	},
+	{
+		"index": 8,
+		"pulse_width_range": [ 600, 2000 ],
+		"actuation_range":   130,
+	},
+]
+
+front_left = [
+	{
+		"index": 15,
+		"pulse_width_range": [ 550, 1300 ],
+		"actuation_range":   90,
+	},
+	{
+		"index": 14,
+		"pulse_width_range": [ 750, 2500 ],
+		"actuation_range":   180,
+	},
+	{
+		"index": 13,
+		"pulse_width_range": [ 600, 2000 ],
+		"actuation_range":   130,
+	},
+]
+
+mid_left = [
+	{
+		"index": 12,
+		"pulse_width_range": [ 550, 1300 ],
+		"actuation_range":   90,
+	},
+	{
+		"index": 11,
+		"pulse_width_range": [ 750, 2500 ],
+		"actuation_range":   180,
+	},
+	{
+		"index": 10,
+		"pulse_width_range": [ 600, 2000 ],
+		"actuation_range":   130,
+	},
+]
+
+back_left = [
+	{
+		"index": 9,
+		"pulse_width_range": [ 1600, 2500 ],
+		"actuation_range":   90,
+	},
+	{
+		"index": 8,
+		"pulse_width_range": [ 1200, 2750 ],
+		"actuation_range":   180,
+	},
+	{
+		"index": 7,
+		"pulse_width_range": [ 500, 2000 ],
+		"actuation_range":   130,
+	},
+]
+
+legs_right = [
+	front_right,
+	mid_right,
+	back_right,
+]
+
+legs_left = [
+	front_left,
+	mid_left,
+	back_left,
+]
+
+for joints in legs_right:
+	for joint in joints:
+		right.servo[joint["index"]].set_pulse_width_range(*joint["pulse_width_range"])
+		right.servo[joint["index"]].actuation_range = joint["actuation_range"]
+
+for joints in legs_left:
+	for joint in joints:
+		left.servo[joint["index"]].set_pulse_width_range(*joint["pulse_width_range"])
+		left.servo[joint["index"]].actuation_range = joint["actuation_range"]
+
 # BACK LEFT
-left.servo[0].set_pulse_width_range(1500, 2700)
-left.servo[0].actuation_range = 100
+left.servo[6].set_pulse_width_range(1500, 2700)
+left.servo[6].actuation_range = 100
 
-left.servo[1].set_pulse_width_range(450, 2400)
-left.servo[1].actuation_range = 180
+left.servo[7].set_pulse_width_range(450, 2400)
+left.servo[7].actuation_range = 180
 
-left.servo[2].set_pulse_width_range(500, 2000)
-left.servo[2].actuation_range = 130
-
-# FRONT RIGHT
-right.servo[6].set_pulse_width_range(1600, 2500)
-right.servo[6].actuation_range = 90
-
-right.servo[7].set_pulse_width_range(1200, 2750) # up, down
-right.servo[7].actuation_range = 180
-
-right.servo[8].set_pulse_width_range(500, 2000) # up, down
-right.servo[8].actuation_range = 130
-
-# BACK RIGHT
-right.servo[0].set_pulse_width_range(550, 1300) # back, front
-right.servo[0].actuation_range = 90
-
-right.servo[1].set_pulse_width_range(750, 2500) # up, down
-right.servo[1].actuation_range = 180
-
-right.servo[2].set_pulse_width_range(600, 2000) # up, down
-right.servo[2].actuation_range = 130
+left.servo[8].set_pulse_width_range(500, 2000)
+left.servo[8].actuation_range = 130
 
 def blReset():
 	left.servo[0].angle = 45
@@ -49,14 +159,14 @@ def frReset():
 	right.servo[7].angle = 135
 	right.servo[8].angle = 90
 
-frReset()
+#  frReset()
 
-#  servo_to_test = 8
+servo_to_test = 0
 
-#  time.sleep( 2 )
+time.sleep( 2 )
 
-#  right.servo[servo_to_test].angle = 180
+right.servo[servo_to_test].angle = 90
 
-#  time.sleep( 2 )
+time.sleep( 2 )
 
-#  right.servo[servo_to_test].angle = 0
+right.servo[servo_to_test].angle = 0
