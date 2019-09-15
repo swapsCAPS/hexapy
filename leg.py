936e2ef096066057c4545643802a12659b293d83
swapsCAPS <verstegen.daan@gmail.com>
2019-09-15 16:26:28 +0100

    baby steps! w00t!

--- leg.py
+++ leg.py
@@ -1,27 +1,24 @@
 import time
 
 class Joint:
-	def __init__(self, side, is_calibrated, index, pulse_width_range, actuation_range, resting):
+	def __init__(self, side, is_calibrated, index, pulse_width_range, actuation_range, positions):
 		self.side = side
 
-		print(index)
-		print(pulse_width_range)
-		print(actuation_range)
-		print(resting)
 		self.servo = self.side.servo[index]
 
 		self.servo.set_pulse_width_range(*pulse_width_range)
 
 		self.servo.actuation_range = actuation_range
 
-		self.positions = {
-			"resting": resting
-		}
+		self.positions = positions
 
 		self.servo.angle = None
 
-	def set_resting_pose(self):
-		self.servo.angle = self.positions["resting"]
+	def set_position(self, position):
+		self.servo.angle = self.positions[position]
+
+	def set_rested_pose(self):
+		self.servo.angle = self.positions["rested"]
 
 	def disable(self):
 		self.servo.angle = None
@@ -30,28 +27,27 @@ class Joint:
 		actuation_range = self.servo.actuation_range
 		angle           = self.servo.angle
 
-		angle           = angle or self.positions["resting"]
-
-		print(angle)
+		angle           = angle or self.positions["rested"]
 
 		for i in range(angle, actuation_range):
 			self.servo.angle = i
-			print(i)
 			time.sleep(0.03)
 		for i in range(actuation_range, angle, -1):
 			self.servo.angle = i
-			print(i)
 			time.sleep(0.03)
+
+# For left and for right we have 3 types of legs
+# So we should be able to define movements in a pretty abstract way
+# is_left could define the step direction.
+# Pelvis is the important one. Can we just use 'actuation_range'? Not really as we go too far sometimes.
+# But then we could also recalibrate to have back and front legs make smaller steps
 
 class Leg:
 	def __init__(self, side, pelvis, knee, ankle):
 		self.side = side
 
-		print('creating pelvis', pelvis)
 		self.pelvis = Joint(side, **pelvis)
-		print('creating knee  ', knee)
 		self.knee   = Joint(side, **knee)
-		print('creating ankle ', ankle)
 		self.ankle  = Joint(side, **ankle)
 
 		self.joints = [
@@ -60,9 +56,23 @@ class Leg:
 			self.ankle,
 		]
 
-	def set_resting_pose(self):
+	def move_forwards(self):
+		self.pelvis.set_position("stepped_front")
+
+	def move_backwards(self):
+		self.pelvis.set_position("stepped_back")
+
+	def lower(self):
+		self.knee.set_position("rested")
+		self.ankle.set_position("rested")
+
+	def lift(self):
+		self.knee.set_position("lifted")
+		self.ankle.set_position("lifted")
+
+	def set_rested_pose(self):
 		for joint in self.joints:
-			joint.set_resting_pose()
+			joint.set_rested_pose()
 
 	def disable(self):
 		for joint in self.joints:
--- main.py
+++ main.py
@@ -5,27 +5,57 @@ from leg import Leg
 left  = ServoKit(channels=16, address=0x40)
 right = ServoKit(channels=16, address=0x41)
 
+positions_right = {
+	"knee": {
+		"lifted": 20, # higher num is higher lift
+		"rested": 0,
+	},
+	"ankle": {
+		"lifted": 65,
+		"rested": 30,
+	}
+}
+
+positions_left = {
+	"knee": {
+		"lifted": 110, # lower num is higher lift
+		"rested": 130,
+	},
+	"ankle": {
+		"lifted": 65,
+		"rested": 90,
+	}
+}
+
 front_right_config = {
 	"pelvis": {
 		"is_calibrated":     True,
 		"index":             0,
 		"pulse_width_range": [ 1600, 2500 ], # first is backward
 		"actuation_range":   90,
-		"resting":           60,
+		"positions": {
+			"rested":        60,
+			"stepped_front": 60,
+			"stepped_back":  40,
+		}
 	},
 	"knee": {
 		"is_calibrated":     True,
 		"index":             1,
 		"pulse_width_range": [ 450, 2000 ], # first is downward
 		"actuation_range":   130,
-		"resting":           0,
+		"positions": {
+			**positions_right["knee"],
+		}
 	},
 	"ankle": {
 		"is_calibrated":     True,
 		"index":             2,
 		"pulse_width_range": [ 500, 2000 ], # first is inward
 		"actuation_range":   130,
-		"resting":           30,
+		"positions": {
+			**positions_right["ankle"],
+		}
 	}
 }
 
@@ -35,21 +65,29 @@ mid_right_config = {
 		"index": 3,
 		"pulse_width_range": [ 1300, 1900 ],
 		"actuation_range":   90,
-		"resting":           45,
+		"positions": {
+			"rested":       45,
+			"stepped_front": 60,
+			"stepped_back":  30,
+		}
 	},
 	"knee": {
 		"is_calibrated":     True,
 		"index": 4,
 		"pulse_width_range": [ 510, 2000 ],
 		"actuation_range":   130,
-		"resting":           0,
+		"positions": {
+			**positions_right["knee"],
+		}
 	},
 	"ankle": {
 		"is_calibrated":     True,
 		"index": 5,
 		"pulse_width_range": [ 550, 2200 ],
 		"actuation_range":   130,
-		"resting":           30,
+		"positions": {
+			**positions_right["ankle"],
+		}
 	}
 }
 
@@ -59,21 +97,29 @@ back_right_config = {
 		"index": 6,
 		"pulse_width_range": [ 450, 1380 ],
 		"actuation_range":   90,
-		"resting":           45,
+		"positions": {
+			"rested":        45,
+			"stepped_front": 60,
+			"stepped_back":  40,
+		}
 	},
 	"knee": {
 		"is_calibrated":     True,
 		"index": 7,
 		"pulse_width_range": [ 460, 2000 ],
 		"actuation_range":   180,
-		"resting":           0,
+		"positions": {
+			**positions_right["knee"],
+		}
 	},
 	"ankle": {
 		"is_calibrated":     True,
 		"index": 8,
 		"pulse_width_range": [ 750, 2050 ],
 		"actuation_range":   130,
-		"resting":           30,
+		"positions": {
+			**positions_right["ankle"],
+		}
 	}
 }
 
@@ -83,21 +129,30 @@ front_left_config = {
 		"index": 15,
 		"pulse_width_range": [ 550, 1350 ], # first is forward
 		"actuation_range":   90,
-		"resting":           45,
+		"positions": {
+			"rested":       45,
+			"stepped_front": 40,
+			"stepped_back":  60,
+		}
 	},
 	"knee": {
 		"is_calibrated":     True,
 		"index": 14,
 		"pulse_width_range": [ 900, 2380 ], # first is upward
 		"actuation_range":   130,
-		"resting":           130,
+		"positions": {
+			**positions_left["knee"],
+			"rested": 130,
+		}
 	},
 	"ankle": {
 		"is_calibrated":     True,
 		"index": 13,
 		"pulse_width_range": [ 1100, 2650 ], # first is outward
 		"actuation_range":   130,
-		"resting":           90,
+		"positions": {
+			**positions_left["ankle"],
+		}
 	}
 }
 
@@ -107,66 +162,177 @@ mid_left_config = {
 		"index": 12,
 		"pulse_width_range": [ 1300, 1900 ],
 		"actuation_range":   90,
-		"resting":           45,
+		"positions": {
+			"rested":       45,
+			"stepped_front": 30,
+			"stepped_back":  60,
+		}
 	},
 	"knee": {
 		"is_calibrated":     True,
 		"index": 11,
 		"pulse_width_range": [ 900, 2550 ], # first is upward
 		"actuation_range":   130,
-		"resting":           130,
+		"positions": {
+			**positions_left["knee"],
+		}
 	},
 	"ankle": {
 		"is_calibrated":     True,
 		"index": 10,
 		"pulse_width_range": [ 1000, 2650 ],
 		"actuation_range":   130,
-		"resting":           90,
+		"positions": {
+			**positions_left["ankle"],
+		}
 	}
 }
 
 back_left_config = {
 	"pelvis": {
 		"is_calibrated":     True,
-		"index": 9,
+		"index":             9,
 		"pulse_width_range": [ 1580, 2500 ],
 		"actuation_range":   90,
-		"resting":           60,
+		"positions": {
+			"rested":       60,
+			"stepped_front": 40,
+			"stepped_back":  60,
+		}
 	},
 	"knee": {
 		"is_calibrated":     True,
-		"index": 8,
+		"index":             8,
 		"pulse_width_range": [ 1200, 2730 ], # first is upward
 		"actuation_range":   130,
-		"resting":           130,
+		"positions": {
+			**positions_left["knee"],
+		}
 	},
 	"ankle": {
 		"is_calibrated":     True,
-		"index": 7,
+		"index":             7,
 		"pulse_width_range": [ 1200, 2650 ],
 		"actuation_range":   130,
-		"resting":           90,
+		"positions": {
+			**positions_left["ankle"],
+		}
 	}
 }
 
-print('front_right')
 front_right = Leg(right, **front_right_config)
-print('mid_right')
 mid_right   = Leg(right, **mid_right_config)
-print('back_right')
 back_right  = Leg(right, **back_right_config)
 
-print('front_left')
 front_left = Leg(left, **front_left_config)
-print('mid_left')
 mid_left   = Leg(left, **mid_left_config)
-print('back_left')
 back_left  = Leg(left, **back_left_config)
 
-# TODO state for each joint
+legs = [
+	front_right,
+	mid_right,
+	back_right,
+	front_left,
+	mid_left,
+	back_left,
+]
+
+set_a = [
+	front_right,
+	back_right,
+	mid_left,
+]
+
+set_b = [
+	mid_right,
+	front_left,
+	back_left,
+]
+
+def move_set_a_forward():
+	for leg in set_a:
+		leg.move_forwards()
+
+def move_set_a_backward():
+	for leg in set_a:
+		leg.move_backwards()
+
+def lift_set_a():
+	for leg in set_a:
+		leg.lift()
+
+def lower_set_a():
+	for leg in set_a:
+		leg.lower()
+
+def move_set_b_forward():
+	for leg in set_b:
+		leg.move_forwards()
+
+def move_set_b_backward():
+	for leg in set_b:
+		leg.move_backwards()
+
+def lift_set_b():
+	for leg in set_b:
+		leg.lift()
+
+def lower_set_b():
+	for leg in set_b:
+		leg.lower()
+
 # TODO Slow move
 
-time.sleep(1)
+for leg in legs:
+	leg.set_rested_pose()
 
-mid_right.set_resting_pose()
-time.sleep(1)
+time.sleep(0.5)
+
+lift_set_a()
+
+time.sleep(0.5)
+
+move_set_b_backward()
+time.sleep(0.1)
+move_set_a_forward()
+
+time.sleep(0.5)
+
+for i in range(4):
+	lower_set_a()
+
+	time.sleep(0.5)
+
+	lift_set_b()
+
+	time.sleep(0.5)
+
+	move_set_a_backward()
+	time.sleep(0.1)
+	move_set_b_forward()
+
+	time.sleep(0.5)
+
+	lower_set_b()
+
+	time.sleep(0.5)
+
+	lift_set_a()
+
+	time.sleep(0.5)
+
+	move_set_a_forward()
+	time.sleep(0.1)
+	move_set_b_backward()
+
+	time.sleep(0.5)
+
+time.sleep(0.5)
+
+for leg in legs:
+	leg.set_rested_pose()
+
+time.sleep(0.5)
+
+for leg in legs:
+	leg.disable()
